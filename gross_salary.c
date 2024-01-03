41dc1e62de59c84912dfff210cc8328fd632843e
Prayush Hada <reachprayush@gmail.com>
2024-01-03 15:02:48 +0545

    gross salary calculation

---
+++ gross_salary.c
@@ -0,0 +1,10 @@
+//WAP to calculate the gross salary of the employee
+#include <stdio.h>
+
+int main(){
+    int basic = 15000;
+    int gross, ssf = 0.30 * basic, allowance = 0.40 * basic;
+    gross = basic + ssf + allowance;
+    printf("Gross Salary: %d\n",gross);
+    return 0;
+}
