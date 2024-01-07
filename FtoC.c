4879b14906a7b6448e226dd017b06c51c7237f7f
Prayush Hada <reachprayush@gmail.com>
2024-01-07 18:50:16 +0545

    error fix

--- FtoC.c
+++ FtoC.c
@@ -4,7 +4,7 @@
 int main(){
     float f,c;
     printf("Enter the temperature in Fahrenheit: ");
-    scanf("%f98.6",&f);
+    scanf("%f",&f);
     c = (f-32)*5/9;
     printf("\nTemperature in Celsius is %0.1f ",c);
     return 0;
