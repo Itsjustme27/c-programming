7831879984e9c201a9cd2a7d4001a42d430bdcee
Prayush Hada <reachprayush@gmail.com>
2024-01-28 17:21:35 +0545

    sum of digits of 4 digit number

---
+++ four_digit.c
@@ -0,0 +1,14 @@
+//WAP to input 4 digits number then calculate the sum of the digits.
+#include<stdio.h>
+
+void main(){
+    int n, sum;
+    printf("Enter a 4 digit number: ");
+    scanf("%d", &n);
+    do{
+        sum += n % 10;
+        n /= 10;
+    }
+    while(n > 0);
+    printf("sum: %d", sum);
+}
