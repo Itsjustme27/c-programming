c2cd6d879a2a4c7e225d349c3a08639c7fdc9edb
Prayush Hada <reachprayush@gmail.com>
2024-01-28 14:43:14 +0545

    sum of two num

---
+++ sum.c
@@ -0,0 +1,9 @@
+//WAP to calculate sum of two input numbers.
+#include <stdio.h>
+
+void main() {
+    int a,b;
+    printf("Enter a and b: ");
+    scanf("%d %d", &a,&b);
+    printf("%d", a+b);
+}
