3881188a34e0f286d2e3ca992d526afaa36e5ae0
Prayush Hada <reachprayush@gmail.com>
2024-02-23 14:42:35 +0545

    factorial recursion

---
+++ user-defined-functions/recursion/factorial.c
@@ -0,0 +1,19 @@
+//factorial
+
+#include <stdio.h>
+
+int fact(int n);
+
+int main() {
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    printf("The factorial is : %d", fact(n));
+    return 0;
+}
+int fact(int n){
+    if(n == 0)
+        return 1;
+    else    
+        return fact(n-1) * n;
+}
