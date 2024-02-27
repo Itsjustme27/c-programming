db107a57518c4bb8d2ff8310bfa8a9b9fa2a57c0
Prayush Hada <reachprayush@gmail.com>
2024-02-27 16:51:28 +0545

    2024/2/27

--- structure/ten_students.c
+++ structure/ten_students.c
@@ -3,11 +3,11 @@
 struct student{
     int roll, marks[5];
     char name[60];
-}s[2];
+}s[10];
 
 int main() {
     int i,j, total, per;
-    for(i = 0; i < 2; i++){
+    for(i = 0; i < 10; i++){
         printf("Enter the roll: ");
         scanf("%d", &s[i].roll);
         printf("Enter the name: ");
@@ -19,7 +19,7 @@ int main() {
     }
 
     // calculating total and percentage of each
-    for(i = 0; i < 2; i++) {
+    for(i = 0; i < 10; i++) {
         total = 0;
         for(j = 0; j < 5; j++){
         total += s[i].marks[j];
@@ -27,7 +27,7 @@ int main() {
         per = total / 5;
     }
     //Displaying result
-    for(i = 0; i < 2; i++){
+    for(i = 0; i < 10; i++){
         printf("Roll: %d\t Name: %s\t Total Marks: %d\t Percentage: %d\n",  s[i].roll, s[i].name, total, per);
     }
     return 0;
